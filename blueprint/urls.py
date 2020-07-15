9bde446cee40802e0cb8b464dc9ce7b01c627d55
Jimmy Carney <jimmy.carney@convr.com>
2020-07-14 22:00:15 -0500

    put login in

--- blueprint/urls.py
+++ blueprint/urls.py
@@ -35,5 +35,5 @@ urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += [
     path('accounts/', include('django.contrib.auth.urls')),
-    #path('accounts/login/', auth_views.LoginView.as_view()),
+    path('accounts/login/', auth_views.LoginView.as_view()),
 ]
